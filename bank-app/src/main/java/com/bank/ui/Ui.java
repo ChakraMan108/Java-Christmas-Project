834c915249a304d8b10969674282aab105ca5cf2
Robert O'D <rod.ledge@gmail.com>
2023-12-15 16:33:06 +0000

    Update Ui.java
    I'm not entirely sure what I have to implement. Is it a GUI?

--- bank-app/src/main/java/com/bank/ui/Ui.java
+++ bank-app/src/main/java/com/bank/ui/Ui.java
@@ -11,3 +11,19 @@ public interface Ui {
 	public long getLong() throws MenuException;
 	public <T> void displayCollection(Collection<T> collection) throws MenuException;
 }
+
+public class Uix implements Ui {
+	public MyFrame() {
+        setTitle("My Frame");
+        setSize(300, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        new MyFrame();
+    }
+}
+	ArrayList <BankAccount> accounts = new ArrayList<BankAccount>();
+}
