234c17b9c16f45203697845abe7d5cbeda87a136
Tom <147619589+ChakraMan108@users.noreply.github.com>
2023-12-15 15:42:52 +0000

    Simplified service & repository interface to save() only

--- bank-app/src/main/java/com/bank/repository/Repository.java
+++ bank-app/src/main/java/com/bank/repository/Repository.java
@@ -8,6 +8,5 @@ public interface Repository<T> {
 	public List<T> findAll() throws RepositoryException; 
 	public T findById(int id) throws RepositoryException;
 	public long count() throws RepositoryException;
-	public void add(T entity) throws RepositoryException;
-	public void update(int id, T entity) throws RepositoryException;
+	public void save(T entity) throws RepositoryException;
 }
