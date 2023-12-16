02e7f1e894f1f5261f8cfc5aec9a968361ff67c3
Tom <147619589+ChakraMan108@users.noreply.github.com>
2023-12-16 17:53:19 +0000

    Customer and CustomerException v1
    First version of the Customer class. Doesn't validate any constructor fields apart from the email.
    
    Field validation will be done in Service/UI layer.

--- bank-app/src/main/java/com/bank/main/Main.java
+++ bank-app/src/main/java/com/bank/main/Main.java
@@ -1,8 +1,14 @@
 package com.bank.main;
 
+import java.time.LocalDate;
+import com.bank.entity.Customer;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+            // Customer cust = new Customer("Joe", "Beech Park", LocalDate.parse("2000-04-15"), "085111222", "tom@x.com", Customer.CustomerType.INDIVIDUAL);
+            // System.out.println(cust);
+
     }
 
 }
