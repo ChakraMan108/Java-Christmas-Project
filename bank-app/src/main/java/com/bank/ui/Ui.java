42e7cbe69a7888635a082230d954f75a97c40707
Robert O'D <rod.ledge@gmail.com>
2023-12-17 12:01:09 +0000

    Update Ui.java
    UI implementation: Please see my updated code for the User interface.

--- bank-app/src/main/java/com/bank/ui/Ui.java
+++ bank-app/src/main/java/com/bank/ui/Ui.java
@@ -1,5 +1,7 @@
 package com.bank.ui;
 
+import javax.swing.*;
+
 import java.util.Collection;
 
 import com.bank.exceptions.MenuException;
@@ -12,18 +14,46 @@ public interface Ui {
 	public <T> void displayCollection(Collection<T> collection) throws MenuException;
 }
 
-public class Uix implements Ui {
-	public MyFrame() {
-        setTitle("My Frame");
-        setSize(300, 200);
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
+public class UiImpl implements Ui {
+    public void authenticateApp() throws MenuException {
+        System.out.println("Enter your username: ");
+        String username = getString();
+        System.out.println("Enter your password: ");
+        String password = getString();
+        if (username.equals("admin") && password.equals("admin")) {
+            displayMenu();
+        } else {
+            throw new MenuException("Invalid Credentials");
+        }
     }
-
-    public static void main(String[] args) {
-        new MyFrame();
+    public void displayMenu() throws MenuException {
+        System.out.println("1. Create Account");
+        System.out.println("2. Deposit");
+        System.out.println("3. Withdraw");
+        System.out.println("4. Transfer");
+        System.out.println("5. Close Account");
+        System.out.println("6. Display All Accounts");
+        System.out.println("7. Exit");
     }
-}
-	ArrayList <BankAccount> accounts = new ArrayList<BankAccount>();
+    public String getString() throws MenuException {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        if (input == null || input.trim().equals("")) {
+            throw new MenuException("Invalid Input");
+        }
+        return input;
+    }
+    public long getLong() throws MenuException {
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        if (input == null || input.trim().equals("")) {
+            throw new MenuException("Invalid Input");
+        }
+        return Long.parseLong(input);
+    }
+    public <T> void displayCollection(Collection<T> collection) throws MenuException {
+        for (T t : collection) {
+            System.out.println(t);
+        }
+    }
 }
