f7090fa99126f1742b6cdcc3921925656c5ef8d7
HynesAvaya23 <153826235+HynesAvaya23@users.noreply.github.com>
2023-12-17 17:02:17 +0000

    Update Transaction.java

--- bank-app/src/main/java/com/bank/entity/Transaction.java
+++ bank-app/src/main/java/com/bank/entity/Transaction.java
@@ -2,23 +2,27 @@
 //Getter and Setter, Method, Variable
 package com.bank.entity;
 
-import java.time.LocalDate;
+//import java.time.LocalDate;
 import java.util.Date;
+import java.util.Scanner;
 
-import com.bank.entity.Customer;
-import com.bank.entity.Customer.CustomerType;
-import com.bank.entity.BankAccount;
+//import com.bank.entity.Customer;
+//import com.bank.entity.Customer.CustomerType;
+//import com.bank.entity.BankAccount;
+
 
 
 import java.util.ArrayList;
-import com.bank.service.BankAccountService;
-import com.bank.service.TransactionService;
+//import com.bank.service.BankAccountService;
+//import com.bank.service.TransactionService;
 
-//import com.bank.entity.Transaction.TransactionType;
-public class Transaction {
-    public enum TransactionType {
-        DEPOSIT, WITHDRAWAL
+
+enum TransactionType {
+    DEPOSIT, WITHDRAWAL
     }
+
+public class Transaction {
+    
     private String accountName;
     private String accountNumber;
     private long balance;
@@ -27,6 +31,7 @@ public class Transaction {
     private TransactionType transactionType;
     private long transactionId;
     private long transactionAmount;
+    private static long transactionIdCounter = 1;
 
 
 /* ****************************** */
@@ -37,13 +42,12 @@ public class Transaction {
     }
 
     public Transaction(String accountName, String accountNumber, long balance,
-                       Date transactionDate, TransactionType transactionType,
-                       long transactionId, long transactionAmount) {
+                       TransactionType transactionType, long transactionId, long transactionAmount) {
         this.accountName = accountName;
         this.accountNumber = accountNumber;
         this.balance = balance;
-        this.transactionDate = new Date();
-        //this.TransactionType = transactionType;
+        this.transactionDate = new Date(); // Set current date and time
+        this.transactionType = transactionType;
         this.transactionId = transactionId;
         this.transactionAmount = transactionAmount;
     }
@@ -106,14 +110,65 @@ public void setTransactionAmount(long transactionAmount) {
 }
 
 /* ****************************** */
-    public static void main(String[] args) {
-        ArrayList<Transaction> transactions = new ArrayList<>();
+private static long generateTransactionId() {
+    return transactionIdCounter++;
+}
 
+//Scanner here temp. to test if it runs
+public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+
+    // Creating an ArrayList to store Transaction objects
+    ArrayList<Transaction> transactions = new ArrayList<>();
+
+    // Taking user input for transaction details
+    System.out.println("Enter number of transactions:");
+    int numOfTransactions = scanner.nextInt();
+    scanner.nextLine(); // Consume newline
+
+    for (int i = 0; i < numOfTransactions; i++) {
+        System.out.println("Enter account name:");
+        String accountName = scanner.nextLine();
+
+        System.out.println("Enter account number:");
+        String accountNumber = scanner.nextLine();
+
+        System.out.println("Enter balance:");
+        long balance = scanner.nextLong();
+        scanner.nextLine(); // Consume newline
+
+        System.out.println("Enter transaction type (DEPOSIT or WITHDRAWAL):");
+        TransactionType transactionType = TransactionType.valueOf(scanner.nextLine().toUpperCase());
+
+        long transactionId = generateTransactionId(); // Generate transaction ID
+
+        System.out.println("Enter transaction amount:");
+        long transactionAmount = scanner.nextLong();
+        scanner.nextLine(); // Consume newline
+
+        // Creating a new Transaction object and adding it to the ArrayList
+        Transaction transaction = new Transaction(accountName, accountNumber, balance,
+                transactionType, transactionId, transactionAmount);
+        transactions.add(transaction);
+    }
+
+    // Displaying transaction details
+    for (Transaction transaction : transactions) {
+        System.out.println("Account Name: " + transaction.getAccountName());
+        System.out.println("Account Number: " + transaction.getAccountNumber());
+        System.out.println("Balance: " + transaction.getBalance());
+        System.out.println("Transaction Date: " + transaction.getTransactionDate());
+        System.out.println("Transaction Type: " + transaction.getTransactionType());
+        System.out.println("Transaction ID: " + transaction.getTransactionId());
+        System.out.println("Transaction Amount: " + transaction.getTransactionAmount());
+        System.out.println();
+    }
+
+    scanner.close();
+}
+}
   // @Override
    //public String toString() {
-    //    return
-
-   //}
 
 //------------------------OLD IGNORE-----------------------------------
     //BankAccount.getId
@@ -124,12 +179,4 @@ public void setTransactionAmount(long transactionAmount) {
     //Transaction despositTransaction = new Transaction();
 
 
-    //Transaction WithdrawTransaction = new Transaction();
 
-
-
-    //Transaction transferTransaction = new Transaction();
-
-
-}
-}
