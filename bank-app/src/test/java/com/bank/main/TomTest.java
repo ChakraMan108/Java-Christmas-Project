0fd35d086e5a3f9d3dd5f66d15e05da6ca9f2f84
Tom <147619589+ChakraMan108@users.noreply.github.com>
2023-12-20 11:39:22 +0000

    Tests added

--- bank-app/src/test/java/com/bank/main/TomTest.java
+++ bank-app/src/test/java/com/bank/main/TomTest.java
@@ -4,11 +4,10 @@ import java.time.LocalDate;
 
 import com.bank.entity.BankAccount;
 import com.bank.entity.Customer;
-import com.bank.entity.Operation;
-import com.bank.entity.Operation.OperationType;
-import com.bank.entity.Transaction;
-import com.bank.entity.Transaction.TransactionType;
+import com.bank.entity.BankAccount.AccountType;
 import com.bank.service.BankAccountService;
+import com.bank.service.CustomerService;
+import com.bank.service.CustomerService;
 import com.bank.service.OperationService;
 import com.bank.service.TransactionService;
 
@@ -16,16 +15,29 @@ public class TomTest {
             
     public static void main(String[] args) throws Exception {
 
+        // Service initialisation
         BankAccountService baService = new BankAccountService();
         OperationService opService = new OperationService();
         TransactionService trService = new TransactionService();
+        CustomerService cuService = new CustomerService();
+        
+        // Customer creation       
+        Customer c2 = new Customer(1, "Joe", "Beech Park", LocalDate.parse("2000-04-15"), "085111222", "tom@x.com", Customer.CustomerType.INDIVIDUAL);
+        cuService.createCustomer(c2);
+        System.out.println("After customer creation:" + c2);
 
-        Customer c1 = new Customer(1, "Joe", "Beech Park", LocalDate.parse("2000-04-15"), "085111222", "tom@x.com", Customer.CustomerType.INDIVIDUAL);
-        Customer c2 = new Customer(2, "Mary", "Dun Na Mara", LocalDate.parse("1980-08-01"), "087333444", "mary@x.com", Customer.CustomerType.INDIVIDUAL);
-        
-        System.out.println(c1);
-        System.out.println(c2);
-        
+        // Customer deactivation
+        cuService.deactivateCustomer(c2.getId());
+        System.out.println("After customer deactivation:" + c2);
+
+        // Customer Update
+        c2.setEmail("ABC@ABC.ie");
+        cuService.save(c2);
+        System.out.println("After customer upodate:" + c2);
+
+        //Find All Customers
+        System.out.println(cuService.findAll());
+
         // Account Creation
         BankAccount acc2 = new BankAccount();  
         baService.createAccount(acc2);
@@ -34,23 +46,41 @@ public class TomTest {
         // Account Deactivation
         baService.deactivateAccount(acc2.getId());
         System.out.println(acc2);
-
-        System.out.println(baService.findAll());
         
-        // Find All Opearions
-        System.out.println(opService.findAll());
-        baService.saveJson();
+        // Account Update
+        acc2.setType(AccountType.SAVING_ACCOUNT);
+        baService.save(acc2);
 
-        //Bank Account and Transaction testing
+        // Account Deposit
         baService.depositIntoAccount(baService.findById(baService.save(acc2).getId()).getId(), 100000);
         baService.depositIntoAccount(baService.findById(baService.save(acc2).getId()).getId(), 1);
+        
+        //Account Withdrawal
         baService.withdrawFromAccount(baService.findById(baService.save(acc2).getId()).getId(), 50000);
         baService.withdrawFromAccount(baService.findById(baService.save(acc2).getId()).getId(), 50);
 
+        //Find All Accounts
         System.out.println(baService.findAll());
 
+        // Find All Operations
+        System.out.println(opService.findAll());
+        
+        // Find All Transactions
         System.out.println(trService.findAll());
 
+        //Count all Customers
+        System.out.println("Number of customers: " + cuService.count());
+
+        //Count all Accounts
+        System.out.println("Number of accounts: " + baService.count());
+
+        //Count all Transactions
         System.out.println("Number of transactions: " + trService.count());
+
+        //Count all Operations
+        System.out.println("Number of operations: " + opService.count());
+
+        baService.saveJson();
+        
     }
 }
