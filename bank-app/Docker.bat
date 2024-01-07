986fda11b003d19465ffe2abedd9cbd030a4410f
Tom <147619589+ChakraMan108@users.noreply.github.com>
2024-01-07 22:11:35 +0000

    Docker.bat

--- bank-app/Docker.bat
+++ bank-app/Docker.bat
@@ -1,4 +1,3 @@
 cls
 cd "C:\Users\tanakhia\GitHub\Java-Christmas-Project\bank-app\target"
-docker.exe build -t bank-app .
 docker.exe container run -v bank-app-volume:/home/.bank-app-data -it --name bank-app bank-app
